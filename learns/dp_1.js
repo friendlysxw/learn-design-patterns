82fdc0e225203143f77359c6c0b2bb8dbc371f94
shixuewen <friendlysxw@163.com>
2021-05-28 16:41:33 +0800

    初始化仓库

---
+++ learns/dp_1.js
@@ -0,0 +1,8 @@
+/*
+ * @Author: your name
+ * @Date: 2021-05-28 16:23:49
+ * @LastEditTime: 2021-05-28 16:28:35
+ * @LastEditors: Please set LastEditors
+ * @Description: 单例模式
+ * @FilePath: \learn-design-patterns\learns\dp_1.js
+ */
