90a2df4fee5ba1a18644d07322f09b8f207cca87
shixuewen <friendlysxw@163.com>
2021-06-16 15:08:43 +0800

    代理模式：end

--- dp-3/index.js
+++ dp-3/index.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-06-15 15:05:43
- * @LastEditTime: 2021-06-15 15:34:06
+ * @LastEditTime: 2021-06-16 14:40:25
  * @LastEditors: Please set LastEditors
  * @Description: 代理模式
  * @FilePath: \learn-design-patterns\dp-3\index.js
@@ -80,3 +80,64 @@ var B={
         })
     }
 }
+
+console.log('// 缓存代理————start')
+console.log('// 计算乘积')
+var mult = function(){
+    console.log('开始计算乘积');
+    var a=1;
+    for (let i = 0; i < arguments.length; i++) {
+        a=a*arguments[i];
+    }
+    return a;
+}
+console.log(mult(2,3))
+console.log(mult(2,3,4))
+// 加入缓存代理函数
+var proxyMult=(function(){
+    var cache={};
+    return function(){
+        var args=Array.prototype.join.call(arguments,',');
+        if(args in cache){
+            return cache[args];
+        }
+        return cache[args]=mult.apply(this,arguments);
+    }
+})()
+console.log(proxyMult(1,2,3,4));
+console.log(proxyMult(1,2,3,4));
+
+console.log('// 高阶函数动态创建代理————start')
+// 计算乘积
+var mult = function(){
+    var a=1;
+    for (let i = 0; i < arguments.length; i++) {
+        a=a*arguments[i];
+    }
+    return a;
+}
+// 计算加和
+var plus=function(){
+    var a=0;
+    for (let i = 0; i < arguments.length; i++) {
+        a =a+ arguments[i];
+    }
+    return a;
+}
+// 创建缓存代理的工厂
+var createProxyFactory=function(fn){
+    var cache={};
+    return function(){
+        var args=Array.prototype.join.call(arguments,',');
+        if(args in cache){
+            return cache[args];
+        }
+        return cache[args]=fn.apply(this,arguments);
+    }
+}
+var proxyMult=createProxyFactory(mult);
+var proxyPlus=createProxyFactory(plus);
+console.log(proxyMult(1,2,3,4))
+console.log(proxyMult(1,2,3,4))
+console.log(proxyPlus(1,2,3,4))
+console.log(proxyPlus(1,2,3,4))
