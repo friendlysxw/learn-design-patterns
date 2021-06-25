0ff0a852a4ebb6e979bae6788c252486d91e574e
shixuewen <friendlysxw@163.com>
2021-06-25 16:06:58 +0800

    适配器模式：end

---
+++ dp-14/index.js
@@ -0,0 +1,11 @@
+/*
+ * @Author: your name
+ * @Date: 2021-06-25 15:25:08
+ * @LastEditTime: 2021-06-25 16:06:19
+ * @LastEditors: Please set LastEditors
+ * @Description: 适配器模式
+ * @FilePath: \learn-design-patterns\dp-14\index.js
+ * 
+ * 核心：用来解决两个已有接口之间不匹配的问题
+ * 
+ */
